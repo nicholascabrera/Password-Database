634597e6ff21aa376b02ef6e5c34ea865d3a7efe
Nicholas Cabrera <ncabrera528@gmail.com>
2023-10-20 15:06:50 -0400

    added anti-aliasing

--- PasswordDatabase/src/main/java/com/password_db/gui/RoundedBorder.java
+++ PasswordDatabase/src/main/java/com/password_db/gui/RoundedBorder.java
@@ -3,8 +3,10 @@ package com.password_db.gui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.geom.Area;
 import java.awt.geom.RoundRectangle2D;
 
@@ -36,18 +38,25 @@ public class RoundedBorder implements Border {
 
     @Override
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        /* Turn on antialiasing to get nicer ovals. */
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
         Rectangle rect = new Rectangle(0, 0, width, height);
         Area borderRegion = new Area(rect);
         RoundRectangle2D.Double roundRect = new RoundRectangle2D.Double(x, y, width, height, radius, radius);
 
         borderRegion.subtract(new Area(roundRect));
-
+        
         g.setClip(borderRegion);
         g.setColor(this.backgroundColor);
         g.fillRect(0, 0, width, height);
         g.setClip(null);
 
         g.setColor(this.foregroundColor);
+        g.drawRoundRect(x, y, width, height, radius, radius);
+        g.drawRoundRect(x, y, width, height, radius+2, radius+2);
         g.drawRoundRect(x, y, width, height, radius+3, radius+3);
     }
 }
