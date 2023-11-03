cba194ef89b39ff6e8893cc2ab75093ddd261cc5
Nicholas Cabrera <ncabrera528@gmail.com>
2023-11-03 14:38:21 -0400

    updated script

--- db_setup.sql
+++ db_setup.sql
@@ -6,3 +6,10 @@ INSERT INTO users VALUES (1, 'admin');
 CREATE DATABASE IF NOT EXISTS pass_db;
 USE pass_db;
 CREATE TABLE IF NOT EXISTS app_pass (ID INT, password VARCHAR(256), salt VARCHAR(128));
+
+CREATE DATABASE IF NOT EXISTS generated_db;
+USE generated_db;
+
+CREATE TABLE IF NOT EXISTS e_keys (ID varchar(128), e_key VARCHAR(256), salt VARCHAR(256));
+CREATE TABLE IF NOT EXISTS e_keys (ID varchar(128), password VARCHAR(256));
+CREATE TABLE IF NOT EXISTS website_users (ID varchar(128), website VARCHAR(50), username VARCHAR(50));
